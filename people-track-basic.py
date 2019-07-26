ef6463a22a1ee77d76918de47b5fcadfbbbdf242
John Garrett <jgarrett@kabbage.com>
2019-07-26 14:19:36 -0400

    tweaking logic for people counting

--- people-track-basic.py
+++ people-track-basic.py
@@ -1,9 +1,9 @@
-# import the necessary packages
 from imutils.video import VideoStream
 import argparse
 import datetime
 import cv2
 import imutils
+
 args = {}
 args['video'] = 'videos/hallway_cut.mov'
 args['min_area'] = 1000
@@ -19,16 +19,13 @@ while True:
     # grab the current frame and initialize the occupied/unoccupied
     frame = vs.read()
     frame = frame if args.get("video", None) is None else frame[1]
-    text = "Unoccupied"
 
-    # if the frame could not be grabbed, then we have reached the end
-    # of the video
     if frame is None:
         break
     
-    # resize the frame, convert it to grayscale, and blur it
+    # convert to grayscale, and blur it
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray = cv2.bilateralFilter(gray, 7, 50, 50)
+    #gray = cv2.bilateralFilter(gray, 7, 50, 50)
 
     # if the first frame is None, initialize it
     if firstFrame is None:
@@ -43,10 +40,8 @@ while True:
     # dilate the thresholded image to fill in holes, then find contours
     # on thresholded image
     thresh = cv2.dilate(thresh, None, iterations=2)
-    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-                            cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
-
 
     rects = [] # the detected people
 
@@ -66,7 +61,7 @@ while True:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
     # draw the text and timestamp on the frame
-    cv2.putText(frame, "People Count: {}".format(len(rects)), (10, 20),
+    cv2.putText(frame, f'People Count: {len(rects)}', (10, 20),
                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 
     # show the frame and record if the user presses a key
