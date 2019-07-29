cbfe0899b067d1de35089faa1d1055e5b734589a
John Garrett <jgarrett@kabbage.com>
2019-07-29 15:56:26 -0400

    WIP

--- headless-wide-angle.py
+++ headless-wide-angle.py
@@ -1,5 +1,6 @@
 import cv2
 import imutils
+import datetime
 import threading
 import numpy as np
 from flask import Flask
@@ -9,15 +10,15 @@ from imutils.video import VideoStream
 app = Flask(__name__)
 api = Api(app)
 
-class PersonDetection(Resource):
-    persons_count = 0
+persons_count = 0
 
-    def count_people(self):
+class PersonDetection(Resource):
+    def count_people():
         vs = cv2.VideoCapture('https://stream-us1-foxtrot.dropcam.com/nexus_aac/f2a6b836da604bae9ca428635c173814/chunklist_w1121249579.m3u8?public=6F7uwYxcUX')
         refrence_frame = None
         frame_count = 0
         
-        while frame_count < 96:
+        while True:
             frame_count += 1
             _, current_frame = vs.read()
 
@@ -53,16 +54,24 @@ class PersonDetection(Resource):
 
                 rects.append(cv2.boundingRect(contour))
             cv2.groupRectangles(rects, 1, 5)
-            self.persons_count += len(rects)
+            persons_count += len(rects)
 
-        self.display_count = round(self.persons_count / frame_count)
+        display_count = round(persons_count / frame_count)
 
+class LineStatistics(Resource):
+    def line_open():
+        current_time = datetime.datetime.now()
+        if current_time.hour == 11 and current_time.minute > 38:
+            return true
+        elif current_time.hour == 2 and current_time.minute > 2:
+            return true
+        else:
+            return false
     def get(self):
-        thread = threading.Thread(target=self.count_people)
-        thread.start()
-        thread.join()
-        return {'persons': self.display_count }
+        return persons_count
 
-api.add_resource(PersonDetection, '/persons_count')
+
+api.add_resource(LineStatistics, '/')
 if __name__ == '__main__':
+    thread = threading.Thread(target=PersonDetection.count_people).start()
     app.run(debug=True)
