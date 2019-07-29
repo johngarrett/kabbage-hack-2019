3815d1e0f9fd675bed94f7903f19807302af44c9
John Garrett <jgarrett@kabbage.com>
2019-07-29 09:58:22 -0400

    improving recognition

--- wide-angle-count.py
+++ wide-angle-count.py
@@ -1,28 +1,29 @@
 from imutils.video import VideoStream
 import cv2
 import imutils
+import numpy as np
 
-min_area = 1500
-max_area = 8000
+min_area = 2000
+max_area = 5000
 
-vs = cv2.VideoCapture('videos/lunch1-cut.mov')
+vs = cv2.VideoCapture('videos/lunch-wide-cut.mov')
 
 refrence_frame = None
 
-count = 0
+frame_count = 0
 while True:
-    count += 1
-    _, original_frame = vs.read()
+    frame_count += 1
+    _, current_frame = vs.read()
 
-    if original_frame is None:
+    if current_frame is None:
         break
     
     # convert to grayscale, and blur it
-    edited_frame = cv2.cvtColor(original_frame, cv2.COLOR_BGR2GRAY)
+    edited_frame = cv2.cvtColor(current_frame, cv2.COLOR_BGR2GRAY)
     edited_frame = cv2.bilateralFilter(edited_frame, 7, 50, 50)
 
-    # every second, compare the delta
-    if refrence_frame is None or count % 24 == 0:
+    # every 10 seconds, compare the delta
+    if refrence_frame is None or frame_count % 48 == 0:
         refrence_frame = edited_frame
         continue
 
@@ -33,8 +34,8 @@ while True:
 
     # dilate the thresholded image to fill in holes, then find contours
     # on thresholded image
-    thresh = cv2.dilate(thresh, None, iterations = 2)
-    countours = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    thresh = cv2.dilate(thresh, np.ones((5,5), np.uint8), iterations = 1)
+    countours = cv2.findContours(thresh.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_TC89_KCOS)
     countours = imutils.grab_contours(countours)
 
     rects = [] # the detected movement
@@ -47,17 +48,18 @@ while True:
             continue
 
         rects.append(cv2.boundingRect(contour))
-    cv2.groupRectangles(rects, 10, 10)
+    cv2.groupRectangles(rects, 1, 5)
 
     print(rects)
     for (x, y, w, h) in rects:
-        cv2.rectangle(original_frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv2.rectangle(current_frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    
+    if frame_count % 48 != 0:
+        # draw the text and timestamp on the frame
+        cv2.putText(current_frame, f'People Count: {len(rects)}', (90, 90),
+                    cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 0, 255), 5)
 
-    # draw the text and timestamp on the frame
-    cv2.putText(original_frame, f'People Count: {len(rects)}', (10, 20),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-
-    cv2.imshow("Camera Feed", original_frame)
-    #cv2.imshow("Thresh", thresh)
-    #cv2.imshow("Frame Delta", frame_delta)
+    cv2.imshow("Camera Feed", current_frame)
+    cv2.imshow("Thresh", thresh)
+    cv2.imshow("Frame Delta", frame_delta)
     cv2.waitKey(1)
